ad633173f9af7d97edc0d84728db476fe8648b56
Maritza Velasco Esteban <166876606+mvelascoe@users.noreply.github.com>
2024-08-13 06:14:50 -0500

    :tada: initial commit

---
+++ src/main/java/com/spring/filtro/FiltroApplication.java
@@ -0,0 +1,13 @@
+package com.spring.filtro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroApplication.class, args);
+	}
+
+}
